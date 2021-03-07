bebd987d9b805c1a9bc45f6d61c5ad88cfcf2d6b
Raman-Piachko <rpechko88@gmail.com>
2021-03-07 21:08:30 +0300

    Special Collections

--- src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
+++ src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
@@ -1,5 +1,137 @@
 package com.efimchick.ifmo.collections;
 
-class MedianQueue  {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
+class MedianQueue implements Queue<Integer> {
+    private final LinkedList<Integer> queue = new LinkedList<>();
+
+    @Override
+    public boolean add(final Integer integer) {
+        return queue.add(integer);
+    }
+
+    @Override
+    public boolean offer(final Integer integer) {
+        queue.offer(integer);
+        rearrangeElements();
+        return true;
+    }
+
+    @Override
+    public Integer remove() {
+        Integer removed = queue.remove();
+        rearrangeElements();
+        return removed;
+    }
+
+    @Override
+    public Integer poll() {
+        Integer removed = queue.poll();
+        rearrangeElements();
+        return removed;
+    }
+
+    @Override
+    public Integer element() {
+        return queue.element();
+    }
+
+    @Override
+    public Integer peek() {
+        return queue.peek();
+    }
+
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        if (queue.remove(o)) {
+            rearrangeElements();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        return queue.contains(o);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return queue.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return queue.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(final T[] a) {
+        return queue.toArray(a);
+    }
+
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        return queue.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends Integer> c) {
+        queue.addAll(c);
+        rearrangeElements();
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        if (queue.removeAll(c)) {
+            rearrangeElements();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        return queue.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        queue.clear();
+    }
+
+
+    private void rearrangeElements() {
+        LinkedList<Integer> list = new LinkedList<>(queue);
+        Collections.sort(list);
+        clear();
+        int initialSize = list.size();
+        for (int i = 0; i < initialSize; i++) {
+            if (isEven(i)) {
+                queue.addFirst(list.pollLast());
+            } else {
+                queue.addFirst(list.pollFirst());
+            }
+        }
+    }
+
+    private boolean isEven(int number) {
+        return number % 2 == 0;
+    }
 }
--- src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
+++ src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
@@ -1,5 +1,75 @@
 package com.efimchick.ifmo.collections;
 
-class SortedByAbsoluteValueIntegerSet {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 
+class SortedByAbsoluteValueIntegerSet implements Set<Integer> {
+    private final Set<Integer> sortedIntegerSet = new TreeSet<>(new AbsoluteValueIntegerComparator());
+
+    @Override
+    public int size() {
+        return sortedIntegerSet.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return sortedIntegerSet.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return sortedIntegerSet.contains(o);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return sortedIntegerSet.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return sortedIntegerSet.toArray();
+    }
+
+    @Override
+    public boolean add(Integer o) {
+        return sortedIntegerSet.add(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return sortedIntegerSet.remove(o);
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return sortedIntegerSet.addAll(c);
+    }
+
+    @Override
+    public void clear() {
+        sortedIntegerSet.clear();
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return sortedIntegerSet.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return sortedIntegerSet.retainAll(c);
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return sortedIntegerSet.containsAll(c);
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return sortedIntegerSet.toArray(a);
+    }
 }
